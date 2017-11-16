d5770ac45f3ad7efb8a6a0d9dee174fa83c08b2f
zperez00 <rancid80s@csu.fullerton.edu>
2017-11-16 13:36:28 -0800

    attempt to connect to api

--- friends.h
+++ friends.h
@@ -7,29 +7,16 @@
 #ifndef friends_h
 #define friends_h
 
-#include <sqlite3.h>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <cstdlib>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <algorithm>
-
-
-std::vector<std::string> friends;
-
-size_t stringLength = friends.size();
 
 class Friends{
 public:
     Friends();
-    Friends(size_t id, std::string name, std::string picurl);
+    Friends(std::string name, std::string picurl, std::string id);
     
-    void setId(size_t id);
-    size_t getId();
+    void setId(std::string id);
+    std::string getId();
 
     void setNames(std::string fname);
     std::string getNames( ) const;
@@ -37,22 +24,15 @@ public:
     void setPicUrls(std::string pic);
     std::string getPicUrls() const;
     
-    std::string genRandom();
+    int genRandom();
     
     bool compareNames(std::string random, std::string guess);
-
-    void insertBlob();
 	
-    void retrieveBlob();
-
-    void deleteBlob();
-
-    friend int callback(void* data, int argc, char** argv, char** azColName);
     
 private:
     std::string name_;
     std::string picurl_;
-    size_t id_;
+    std::string id_;
 };
 
 
