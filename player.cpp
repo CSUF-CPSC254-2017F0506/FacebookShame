fdd1dbae467d6dd832aef484f5ad4d1b640b0aa4
zperez00 <rancid80s@csu.fullerton.edu>
2017-11-19 02:27:32 -0800

    fixing some database bugs

--- player.cpp
+++ player.cpp
@@ -49,7 +49,7 @@ void Player::createPlayerInfo(){
 	/*Create SQL statement */
 
 	sql = "CREATE TABLE IF NOT EXISTS Player ("\
-		 "ID	 TEXT PRIMARY KEY NOT NULL UNIQUE,"\
+		 "ID	 TEXT PRIMARY KEY NOT NULL,"\
 		 "NAME   TEXT	 NOT NULL);"; \
 		 
 	
@@ -67,10 +67,8 @@ void Player::createPlayerInfo(){
 	}
 
 	sql = "CREATE TABLE IF NOT EXISTS Scores("\
-		 "PID TEXT PRIMARY KEY NOT NULL,"\
-		 "SCORE INTEGER,"
-		 "FOREIGN KEY (PID) REFERENCES Players(ID));";\
-
+		 "PID TEXT NOT NULL,"\
+		 "SCORE INTEGER);";
 	/* Execute SQL statement */
 	rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
 	
@@ -109,7 +107,7 @@ void Player::insertInfo(){
 	}	
 	/* Create SQL statement */
 	
-	sql = "INSERT INTO Player(ID,NAME)"\
+	sql = "INSERT OR REPLACE INTO Player(ID,NAME)"\
 		 " VALUES("+ std::to_string(player_id_) +",'"+ player_name_ +"');";
 
 	/* Execute SQL statement */
@@ -126,8 +124,8 @@ void Player::insertInfo(){
 		fprintf(stdout, "Records created successfully\n");
 	}
 
-	sql = "INSERT INTO Scores(PID,SCORE)"\
-		 " VALUES("+ std::to_string(player_id_) +","+ std::to_string(score_) +");";
+	sql ="INSERT INTO Scores(PID, SCORE)"\
+		" VALUES("+ std::to_string(player_id_) +", "+ std::to_string(score_) +");";
 
 	/* Execute SQL statement */
 	
@@ -151,7 +149,7 @@ void Player::retrieveInfo(){
 	char* zErrMsg = 0;
 	int rc;
 	std::string sql;
-	char* data = 0;
+	Player* data =  new Player();
 
 	/* Open database */
 	
@@ -170,6 +168,7 @@ void Player::retrieveInfo(){
 	sql = "SELECT * FROM Player"
 		 " WHERE ID LIKE \" '"+ std::to_string(player_id_) +"' \";";
 	
+	std::cout<< sql <<std::endl;
 
 	/* Execute SQL statement */
 
@@ -183,6 +182,12 @@ void Player::retrieveInfo(){
 	else{
 		fprintf(stdout, "Operation done successfully\n");
 	}
+
+	player_name_ = data->player_name_;
+	score_ = data->score_;
+	
+	delete data;
+	
 	sqlite3_close(db);
 }
 
@@ -223,17 +228,4 @@ size_t Player::getHighScore()const{
     return high_score_;
 }
 
-int main(){
-	Player p;
-	p.createPlayerInfo();
-	
-	size_t id = 13579;
-	p.setId(id);
-	std::string name = "Zules";
-	p.setName(name);
 
-	size_t score = 0;
-	p.setScore(score);
-	
-	p.insertInfo();
-}
